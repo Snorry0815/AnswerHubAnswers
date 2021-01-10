05510e1da2afc6d52d8a176596aef5de3ffc0c17
Snorry0815 <kane_htl@gmx.at>
2021-01-10 14:08:11 +0100

    some more answers

---
+++ Source/AnswerHubAnswers/TickingCheatManager.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Tickable.h"
+#include "Engine/Engine.h"
+#include "GameFramework/CheatManager.h"
+
+#include "TickingCheatManager.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+
+class UTickingCheatManager : public UCheatManager, public FTickableGameObject
+{
+  GENERATED_BODY()
+
+public:
+  //~ Begin FTickableGameObject interface
+  virtual TStatId GetStatId() const override
+  {
+    RETURN_QUICK_DECLARE_CYCLE_STAT(UTickingCheatManager, STATGROUP_Tickables);
+  }
+  virtual bool IsTickable() const override
+  {
+    return true;
+  }
+
+  virtual void Tick(float DeltaTime) override;
+  //~ End FTickableGameObject interface
+};
+
+inline void UTickingCheatManager::Tick(float DeltaTime)
+{
+  GEngine->AddOnScreenDebugMessage(0, 0, FColor::Green, FString::SanitizeFloat(DeltaTime));
+}
